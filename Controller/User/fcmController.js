7fc39f6aedd07efd58fdcdadbd46f00b9eab5b43
parnets21 <parnetstech21@gmail.com>
2025-09-15 16:17:03 +0530

    qwerty

--- Controller/User/fcmController.js
+++ Controller/User/fcmController.js
@@ -4,9 +4,9 @@ const admin = require("firebase-admin");
 const serviceAccount = require('../../serviceAccountKey.json');
 
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 }
 
 
--- utils/firebase.js
+++ utils/firebase.js
@@ -1,10 +1,10 @@
-// // utils/firebase.js
-// const admin = require("firebase-admin");
-// const serviceAccount = require("../serviceAccountKey.json"); 
-// if (!admin.apps.length) {
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
-// }
+// utils/firebase.js
+const admin = require("firebase-admin");
+const serviceAccount = require("../serviceAccountKey.json"); 
+if (!admin.apps.length) {
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+}
 
-// module.exports = admin;
+module.exports = admin;
