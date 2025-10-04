f489c8aeba88fac060ac2c9c4ebac1e56572a56b
parnets21 <parnetstech21@gmail.com>
2025-10-04 14:35:21 +0530

    deploye

--- Routes/User/fcmRoutes.js
+++ Routes/User/fcmRoutes.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const router = express.Router(); 
-const fcm = require ("../../Controller/User/fcmController");
-
-
-
+const fcm = require("../../Controller/User/fcmController");
 
 router.post("/fcmToken", fcm.sendNotificationToEmployee);
-router.post("/update-token" , fcm.updateFCMToken )
+router.post("/update-token", fcm.updateFCMToken);
 router.post("/clear-token", fcm.clearFCMToken);
 router.post("/bulk-notification", fcm.sendBulkNotification);
 router.get("/token/:userId", fcm.getFCMTokenByUserId);
@@ -15,5 +12,4 @@ router.post("/reactivate-token", fcm.reactivateFCMToken);
 router.delete("/delete-token", fcm.deleteFCMToken);
 router.get("/stats", fcm.getFCMTokenStats);
 
-module.exports = router; 
-
+module.exports = router;
