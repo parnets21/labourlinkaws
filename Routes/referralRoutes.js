28758131c96c0fcfdcf890ead59d125ba5cfaba2
jonathan-armal <kirankumar47230@gmail.com>
2026-05-04 15:52:04 +0530

    refferal

--- Routes/referralRoutes.js
+++ Routes/referralRoutes.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const referralController = require('../Controller/referralController');
 const { protect } = require('../Controller/authController');
 
+// Public routes (no auth required)
+router.get('/validate/:code', referralController.validateReferralCode);
+router.get('/settings', referralController.getReferralSettings);
+
 // User routes (require authentication)
 router.get('/code', protect, referralController.getReferralCode);
 router.post('/create-by-code', protect, referralController.createReferralByCode);
 router.get('/stats', protect, referralController.getReferralStats);
 router.get('/history', protect, referralController.getReferralHistory);
-
-// Public route (no auth required)
-router.get('/validate/:code', referralController.validateReferralCode);
 
 module.exports = router;
